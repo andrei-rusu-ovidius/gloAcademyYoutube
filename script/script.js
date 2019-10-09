9e2268e7ab3a5765c9a9638623bc53eabf3a3935
Artem <Erlementovich@yandex.ru>
2019-10-09 23:01:49 +0300

    gloAcademy Day2

--- script/script.js
+++ script/script.js
@@ -10,13 +10,40 @@ document.addEventListener('DOMContentLoaded', () => {
         const toggleKeyboard = () => {
             keyboard.style.top = keyboard.style.top ? "" : "50%";
         }
+        const changeLang = (btn, lang) => {
+            const langRu = ['ё', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '-', '=', '⬅',
+                'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ',
+                'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э',
+                'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.',
+                'en', ' '
+            ];
+            const langEn = ['`', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '-', '=', '⬅',
+                'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']',
+                'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '"',
+                'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/',
+                'ru', ' '
+            ];
+
+            if (lang === "en") {
+                btn.forEach((elem, i) => {
+                    elem.textContent = langEn[i];
+                })
+            } else if (lang === "ru") {
+                btn.forEach((elem, i) => {
+                    elem.textContent = langRu[i];
+                })
+            }
+        }
+
         const typing = event => {
             const target = event.target;
-
-            if (target.tagName.toLowerCase() === "button" && target.id != "keyboard-backspace" && target.style.width != "293px") {
+            const buttons = [...keyboard.querySelectorAll('button')]
+                .filter(elem => elem.style.visibility != "hidden");
+            const contentButton = target.textContent.trim();
+            if (target.tagName.toLowerCase() === "button" && target.id != "keyboard-backspace" && target.style.width != "293px" && contentButton != "en" && contentButton != "ru") {
 
                 searchInput.value += target.textContent.trim();
-                
+
             } else if (target.id == "keyboard-backspace") {
 
                 searchInput.value = searchInput.value.substring(0, searchInput.value.length - 1);
@@ -25,6 +52,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
                 searchInput.value += " ";
 
+            } else if (contentButton === "en" || contentButton === "ru") {
+                changeLang(buttons, contentButton)
             }
 
 
@@ -61,9 +90,84 @@ document.addEventListener('DOMContentLoaded', () => {
         })
     }
 
+
+    /*
+    
+     */
     //Модальное окно
     {
-        const divYoutuber = document.createElement('div');
+        document.body.insertAdjacentHTML('beforeend', `<div class="youTuberModal">
+    <div id="youtuberClose">&#215;</div>
+    <div id="youtuberContainer"></div>
+    </div>`);
+        const youtuberItems = document.querySelectorAll("[data-youtuber]");
+        const youTuberModal = document.querySelector('.youTuberModal');
+        const youtuberContainer = document.getElementById('youtuberContainer');
+
+
+        const qw = [3840, 2560, 1920, 1280, 854, 640, 426, 256];
+        const qh = [2160, 1440, 1080, 720, 480, 360, 240, 144]; 
+        const sizeVideo = () => {
+
+            let ww = document.documentElement.clientWidth;
+            let wh = document.documentElement.clientHeight;
+            for (let i = 0; i < qw.length; i++) {
+                if (ww > qw[i]) {
+                    youtuberContainer.querySelector('iframe').style.cssText = `
+                    width: ${qw[i]}px;
+                    height: ${qh[i]}px;
+                    `;
+                    youtuberContainer.style.cssText = `
+                    width: ${qw[i]}px;
+                    height: ${qh[i]}px;
+                    top: ${(wh - qh[i]) / 2}px;
+                    left: ${(ww - qw[i]) / 2}px;
+                    `;
+                    break;
+                }
+            }
+
+        };
+
+
+        youtuberItems.forEach(elem => {
+            elem.addEventListener('click', () => {
+                const idVideo = elem.dataset.youtuber;
+                youTuberModal.style.display = "block";
+
+                const youTuberFrame = document.createElement('iframe');
+                youTuberFrame.src = `https://youtube.com/embed/${idVideo}`;
+
+                youtuberContainer.insertAdjacentElement('beforeend', youTuberFrame);
+                window.addEventListener('resize', sizeVideo);
+
+                sizeVideo();
+            })
+
+        })
+
+        youTuberModal.addEventListener('click', () => {
+            youTuberModal.style.display = "";
+            youtuberContainer.textContent = "";
+            window.removeEventListener('resize', sizeVideo);
+        })
+
+
+        //youTube
+
+        {
+            const API_KEY = 'AIzaSyAkRAWykvc-AmFxAt8E_S52Ghim4ojNdzM';
+
+        }
+
+
+
+
+
+
+
+
+
 
     }
 
